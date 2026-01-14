74088e2b6f2c911f197eaa168216b1df81acd80e
NotAnCookie <160712740+NotAnCookie@users.noreply.github.com>
2026-01-14 18:32:03 +0100

    ADD synonyms for first Definition

--- backend/src/main/java/pl/edu/dictionary/client/dto/Definition.java
+++ backend/src/main/java/pl/edu/dictionary/client/dto/Definition.java
@@ -1,6 +1,9 @@
 package pl.edu.dictionary.client.dto;
 
+import java.util.List;
+
 public class Definition {
     public String definition;
     public String example;
+    public List<String> synonyms;
 }
