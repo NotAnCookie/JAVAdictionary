65e446b29089f3b98931e28a926cfcb5c3cf1d15
xblmati <mraczynski01@gmail.com>
2026-01-19 14:16:17 +0100

    Add DefinitionActivity and implement Parcelable for WordDefinition to be able to pass it between activities

---
+++ frontend/app/src/main/java/pl/edu/dictionary/DefinitionActivity.java
@@ -0,0 +1,101 @@
+package pl.edu.dictionary;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import pl.edu.dictionary.models.WordDefinition;
+
+public class DefinitionActivity extends AppCompatActivity {
+	public static final String WORD_DEFINITION_EXTRA = "word_definition";
+	
+	private ListView definitionsListView;
+	
+	private WordDefinition[] wordDefinitions;
+	private Toolbar actionBar;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_definition);
+		actionBar = findViewById(R.id.toolbar);
+		setSupportActionBar(actionBar);
+		
+		definitionsListView = findViewById(R.id.definitionListView);
+		
+		wordDefinitions = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+				? getIntent().getParcelableArrayExtra(WORD_DEFINITION_EXTRA, WordDefinition.class)
+				: (WordDefinition[]) getIntent().getParcelableArrayExtra(WORD_DEFINITION_EXTRA);
+		
+		if (wordDefinitions == null || wordDefinitions.length == 0) {
+			actionBar.setTitle("Not Found");
+			TextView errorTextView = findViewById(R.id.errorTextView);
+			errorTextView.setText("Word not found.");
+			definitionsListView.setVisibility(View.GONE);
+			return;
+		}
+		
+		displayDefinitions(wordDefinitions);
+		
+	}
+	
+	
+	private void displayDefinitions(@NonNull WordDefinition[] wordDefinitions) {
+		actionBar.setTitle(wordDefinitions[0].getWord());
+		var arrayAdapter = new ArrayAdapter<>(this, R.layout.word_definition, wordDefinitions) {
+			@NonNull
+			@Override
+			public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+				if (convertView == null) {
+					convertView = getLayoutInflater().inflate(R.layout.word_definition, parent, false);
+				}
+				displayDefinition(convertView, wordDefinitions[position]);
+				return convertView;
+			}
+		};
+		definitionsListView.setAdapter(arrayAdapter);
+		
+	}
+	private void displayDefinition(View view, @NonNull WordDefinition wordDefinition) {
+		TextView definitionTextView = view.findViewById(R.id.definitionTextView);
+		TextView synonymsTextView = view.findViewById(R.id.synonymsTextView);
+		
+		definitionTextView.setText(wordDefinition.getDefinition());
+		if (wordDefinition.getSynonyms().isEmpty())
+			synonymsTextView.setVisibility(View.GONE);
+		else
+			synonymsTextView.setText("Synonyms: " + TextUtils.join(", ", wordDefinition.getSynonyms()));
+	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		int itemId = item.getItemId();
+		if (itemId == R.id.action_search) {
+			// Handle search action
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.definition_menu, menu);
+		return true;
+	}
+	
+}
--- frontend/app/src/main/java/pl/edu/dictionary/models/WordDefinition.java
+++ frontend/app/src/main/java/pl/edu/dictionary/models/WordDefinition.java
@@ -1,8 +1,13 @@
 package pl.edu.dictionary.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
 import java.util.List;
 
-public class WordDefinition {
+public class WordDefinition implements Parcelable {
 	private String word;
 	private String definition;
 	private List<String> synonyms;
@@ -10,5 +15,42 @@ public class WordDefinition {
 	public String getWord() { return word; }
 	public String getDefinition() { return definition; }
 	public List<String> getSynonyms() { return synonyms; }
+	
+	public WordDefinition() {}
+	
+	
+	// Parcelable implementation
+	
+	protected WordDefinition(Parcel in) {
+		word = in.readString();
+		definition = in.readString();
+		synonyms = in.createStringArrayList();
+	}
+	
+	public static final Creator<WordDefinition> CREATOR = new Creator<>() {
+		@Override
+		public WordDefinition createFromParcel(Parcel in) {
+			return new WordDefinition(in);
+		}
+		
+		@Override
+		public WordDefinition[] newArray(int size) {
+			return new WordDefinition[size];
+		}
+	};
+	
+	
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+	
+	@Override
+	public void writeToParcel(@NonNull Parcel dest, int flags) {
+		dest.writeString(word);
+		dest.writeString(definition);
+		dest.writeStringList(synonyms);
+	}
+	
 }
 
